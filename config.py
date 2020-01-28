008fa339849e80b8671089a63f944011438ebaca
lambdaofgod <bartczukkuba@gmail.com>
2020-01-29 00:04:59 +0100

    guild config

--- config.py
+++ config.py
@@ -7,26 +7,30 @@ from sklearn import linear_model
 from wikir_text_mining import retriever
 from wikir_text_mining import vectorizer
 
-bm25 = pickle.load(open('artifacts/bm25.pkl', 'rb'))
+relevance_model = pickle.load(open('artifacts/bm25.pkl', 'rb'))
 documents_df = pd.read_csv('wikIR1k/documents.csv', index_col='id_right')
 documents_df['text'] = documents_df['text_right']
 
 
-def predefined_retrievers(retriever_type):
+def predefined_retrievers(retriever_type, classifier_retriever_alpha=0.5):
     if retriever_type == 'word2vec_classifier_retriever':
         word2vec_vectorizer = embeddings.AverageWordEmbeddingsVectorizer.from_gensim_embedding_model()
         return retriever.ClassifierRetriever(
-            bm25=bm25,
+            bm25=relevance_model,
             documents_df=documents_df,
             vectorizer=word2vec_vectorizer,
-            clf=linear_model.LogisticRegression(solver='lbfgs')
+            clf=linear_model.LogisticRegression(solver='lbfgs'),
+            alpha=classifier_retriever_alpha
         )
     elif retriever_type == 'classifier_retriever':
         clasifier_retriever = retriever.ClassifierRetriever(
-            bm25=bm25,
+            bm25=relevance_model,
             documents_df=documents_df,
             vectorizer=vectorizer.BM25Vectorizer(),
-            clf=linear_model.LogisticRegression(penalty='l1', solver='liblinear')
+            clf=linear_model.LogisticRegression(penalty='l1', solver='liblinear'),
+            alpha=classifier_retriever_alpha
         )
         clasifier_retriever.vectorizer.fit(documents_df['text'])
         return clasifier_retriever
+    elif retriever_type == 'baseline':
+        return retriever.BM25Retriever(relevance_model, documents_df)
--- retriever_evaluation_pipeline.py
+++ retriever_evaluation_pipeline.py
@@ -4,13 +4,16 @@ import tqdm
 import config
 from wikir_text_mining import evaluation
 
+
+# parameters used by guild.ai (experiment runner)
 retriever_type = 'word2vec_classifier_retriever'
 subset = 'training'
-retriever = config.predefined_retrievers(retriever_type)
+classifier_retriever_alpha = 0.5
 
 
+# setup
+retriever = config.predefined_retrievers(retriever_type, classifier_retriever_alpha)
 queries_df = pd.read_csv('wikIR1k/{}/queries.csv'.format(subset), index_col='id_left')
-
 qrel_path = 'wikIR1k/{}/qrels'.format(subset)
 evaluator = evaluation.setup_evaluator_from_relevance_file(qrel_path)
 
