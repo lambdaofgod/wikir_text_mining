7cb44fad0f52af4a88a1826e6850a307a71ae07f
lambdaofgod <bartczukkuba@gmail.com>
2020-01-31 00:05:04 +0100

    update config

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import pickle
 
 import pandas as pd
 from mlutil import embeddings
-from sklearn import linear_model, naive_bayes, feature_extraction
+from sklearn import linear_model, naive_bayes, feature_extraction, decomposition
 
 from wikir_text_mining import retrievers
 from wikir_text_mining import vectorizers
@@ -12,7 +12,7 @@ documents_df = pd.read_csv('wikIR1k/documents.csv', index_col='id_right')
 documents_df['text'] = documents_df['text_right']
 
 
-def predefined_retrievers(retriever_type, classifier_retriever_alpha=0.5):
+def predefined_retrievers(retriever_type, alpha=0.5):
     if retriever_type == 'word2vec_classifier_retriever':
         word2vec_vectorizer = embeddings.AverageWordEmbeddingsVectorizer.from_gensim_embedding_model()
         return retrievers.ClassifierRetriever(
@@ -20,7 +20,7 @@ def predefined_retrievers(retriever_type, classifier_retriever_alpha=0.5):
             documents_df=documents_df,
             vectorizer=word2vec_vectorizer,
             clf=linear_model.LogisticRegression(solver='lbfgs'),
-            alpha=classifier_retriever_alpha
+            alpha=alpha
         )
     elif retriever_type == 'classifier_retriever':
         clasifier_retriever = retrievers.ClassifierRetriever(
@@ -28,7 +28,7 @@ def predefined_retrievers(retriever_type, classifier_retriever_alpha=0.5):
             documents_df=documents_df,
             vectorizer=vectorizers.BM25Vectorizer(),
             clf=linear_model.LogisticRegression(penalty='l1', solver='liblinear'),
-            alpha=classifier_retriever_alpha
+            alpha=alpha
         )
         clasifier_retriever.vectorizer.fit(documents_df['text'])
         return clasifier_retriever
@@ -38,11 +38,13 @@ def predefined_retrievers(retriever_type, classifier_retriever_alpha=0.5):
             documents_df=documents_df,
             vectorizer=feature_extraction.text.CountVectorizer(),
             clf=naive_bayes.MultinomialNB(),
-            alpha=classifier_retriever_alpha
+            alpha=alpha
         )
         clasifier_retriever.vectorizer.fit(documents_df['text'])
         return clasifier_retriever
     elif retriever_type == 'topic_model_retriever':
-        pass
+        retriever = pickle.load(open('artifacts/topic_model_retriever.pkl', 'rb'))
+        print(retriever)
+        return retriever
     elif retriever_type == 'baseline':
         return retrievers.BM25Retriever(relevance_model, documents_df)
--- retriever_evaluation_pipeline.py
+++ retriever_evaluation_pipeline.py
@@ -6,13 +6,13 @@ from wikir_text_mining import evaluation
 
 
 # parameters used by guild.ai (experiment runner)
-retriever_type = 'word2vec_classifier_retriever'
+retriever_type = 'topic_model_retriever'
 subset = 'training'
-classifier_retriever_alpha = 0.5
+alpha = 0.5
 
 
 # setup
-retriever = config.predefined_retrievers(retriever_type, classifier_retriever_alpha)
+retriever = config.predefined_retrievers(retriever_type, alpha)
 queries_df = pd.read_csv('wikIR1k/{}/queries.csv'.format(subset), index_col='id_left')
 qrel_path = 'wikIR1k/{}/qrels'.format(subset)
 evaluator = evaluation.setup_evaluator_from_relevance_file(qrel_path)
